a9d2d689e36da6b3922aff76ebbc089aad69ce1d
pat-ern <pat.villarroel@duocuc.cl>
2022-03-21 00:11:06 -0300

    actividad 2 caso 2

--- actividad 2.sql
+++ actividad 2.sql
@@ -82,3 +82,46 @@ BEGIN
     
 END;
     
+
+-- CASO 2
+
+DECLARE
+
+v_run empleado.numrun_emp%TYPE := &RUN;
+
+v_mes_anno VARCHAR2(6);
+v_numrun_emp NUMBER(8);
+v_dvrun_emp CHAR;
+v_nombre_empleado VARCHAR2(40);
+v_nombre_usuario VARCHAR2(10); 
+v_clave_usuario VARCHAR2(20);
+
+BEGIN
+
+SELECT TO_CHAR(SYSDATE, 'MMYYYY') MES_ANNO,
+    numrun_emp NUMRUN_EMP,
+    dvrun_emp DVRUN_EMP,
+    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp NOMBRE_EMPLEADO,
+    SUBSTR(pnombre_emp, 0, 3)||LENGTH(pnombre_emp)||'*'||SUBSTR(TO_CHAR(sueldo_base, '9999999'), -1, 1)||dvrun_emp||ROUND(MONTHS_BETWEEN(SYSDATE, fecha_contrato)/12)||
+    CASE WHEN ROUND(MONTHS_BETWEEN(SYSDATE, fecha_contrato)/12) < 10 THEN 'X'
+        ELSE NULL
+    END NOMBRE_USUARIO,
+    SUBSTR(TO_CHAR(numrun_emp, '99999999'), 4, 1)||(EXTRACT (YEAR FROM fecha_nac))+2||SUBSTR(TO_CHAR(sueldo_base-1, '9999999'), -3, 3)||
+    LOWER(CASE id_estado_civil 
+        WHEN 10 THEN SUBSTR(appaterno_emp, 0, 2)
+        WHEN 60 THEN SUBSTR(appaterno_emp, 0, 2)
+        WHEN 20 THEN SUBSTR(appaterno_emp, 0, 1)||SUBSTR(appaterno_emp, -1, 1)
+        WHEN 30 THEN SUBSTR(appaterno_emp, 0, 1)||SUBSTR(appaterno_emp, -1, 1)
+        WHEN 40 THEN SUBSTR(appaterno_emp, -3, 2)
+        WHEN 50 THEN SUBSTR(appaterno_emp, -2, 2)
+    END)||
+    TO_CHAR(SYSDATE, 'MMYYYY')||SUBSTR(nombre_comuna, 0, 1) CLAVE_USUARIO
+        INTO v_mes_anno, v_numrun_emp, v_dvrun_emp, v_nombre_empleado, v_nombre_usuario, v_clave_usuario
+    FROM empleado NATURAL JOIN comuna
+    WHERE numrun_emp = v_run;
+
+INSERT INTO usuario_clave 
+VALUES (v_mes_anno, v_numrun_emp, v_dvrun_emp, v_nombre_empleado, v_nombre_usuario, v_clave_usuario);
+COMMIT;
+    
+END;
