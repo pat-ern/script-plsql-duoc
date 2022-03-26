0f9ee74bdf7ede7bd4cfd5e5e0cbe8510c6365a3
pat-ern <pat.villarroel@duocuc.cl>
2022-03-26 01:39:12 -0300

    desarrollo actividad 2 completo

--- actividad 2.sql
+++ actividad 2.sql
@@ -28,6 +28,7 @@ BEGIN
         apmaterno_emp,
         sueldo_base,
         nombre_comuna
+		
         INTO v_numrun_emp, 
             v_dvrun_emp, 
             v_pnombre_emp, 
@@ -95,30 +96,170 @@ v_clave_usuario VARCHAR2(20);
 
 BEGIN
 
-SELECT TO_CHAR(SYSDATE, 'MMYYYY') MES_ANNO,
-    numrun_emp NUMRUN_EMP,
-    dvrun_emp DVRUN_EMP,
-    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp NOMBRE_EMPLEADO,
-    SUBSTR(pnombre_emp, 0, 3)||LENGTH(pnombre_emp)||'*'||SUBSTR(TO_CHAR(sueldo_base, '9999999'), -1, 1)||dvrun_emp||ROUND(MONTHS_BETWEEN(SYSDATE, fecha_contrato)/12)||
-    CASE WHEN ROUND(MONTHS_BETWEEN(SYSDATE, fecha_contrato)/12) < 10 THEN 'X'
-        ELSE NULL
-    END NOMBRE_USUARIO,
-    SUBSTR(TO_CHAR(numrun_emp, '99999999'), 4, 1)||(EXTRACT (YEAR FROM fecha_nac))+2||SUBSTR(TO_CHAR(sueldo_base-1, '9999999'), -3, 3)||
-    LOWER(CASE id_estado_civil 
-        WHEN 10 THEN SUBSTR(appaterno_emp, 0, 2)
-        WHEN 60 THEN SUBSTR(appaterno_emp, 0, 2)
-        WHEN 20 THEN SUBSTR(appaterno_emp, 0, 1)||SUBSTR(appaterno_emp, -1, 1)
-        WHEN 30 THEN SUBSTR(appaterno_emp, 0, 1)||SUBSTR(appaterno_emp, -1, 1)
-        WHEN 40 THEN SUBSTR(appaterno_emp, -3, 2)
-        WHEN 50 THEN SUBSTR(appaterno_emp, -2, 2)
-    END)||
-    TO_CHAR(SYSDATE, 'MMYYYY')||SUBSTR(nombre_comuna, 0, 1) CLAVE_USUARIO
-        INTO v_mes_anno, v_numrun_emp, v_dvrun_emp, v_nombre_empleado, v_nombre_usuario, v_clave_usuario
-    FROM empleado NATURAL JOIN comuna
-    WHERE numrun_emp = v_run;
+	SELECT TO_CHAR(SYSDATE, 'MMYYYY'),
+		numrun_emp,
+		dvrun_emp,
+		pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp,
+		SUBSTR(pnombre_emp, 0, 3)||LENGTH(pnombre_emp)||'*'||SUBSTR(TO_CHAR(sueldo_base, '9999999'), -1, 1)||dvrun_emp||ROUND(MONTHS_BETWEEN(SYSDATE, fecha_contrato)/12)||
+		CASE WHEN ROUND(MONTHS_BETWEEN(SYSDATE, fecha_contrato)/12) < 10 THEN 'X'
+			ELSE NULL
+		END,
+		SUBSTR(TO_CHAR(numrun_emp, '99999999'), 4, 1)||(EXTRACT (YEAR FROM fecha_nac))+2||SUBSTR(TO_CHAR(sueldo_base-1, '9999999'), -3, 3)||
+		LOWER(CASE id_estado_civil 
+			WHEN 10 THEN SUBSTR(appaterno_emp, 0, 2)
+			WHEN 60 THEN SUBSTR(appaterno_emp, 0, 2)
+			WHEN 20 THEN SUBSTR(appaterno_emp, 0, 1)||SUBSTR(appaterno_emp, -1, 1)
+			WHEN 30 THEN SUBSTR(appaterno_emp, 0, 1)||SUBSTR(appaterno_emp, -1, 1)
+			WHEN 40 THEN SUBSTR(appaterno_emp, -3, 2)
+			WHEN 50 THEN SUBSTR(appaterno_emp, -2, 2)
+		END)||
+		TO_CHAR(SYSDATE, 'MMYYYY')||SUBSTR(nombre_comuna, 0, 1)
+		
+		INTO v_mes_anno, 
+			v_numrun_emp, 
+			v_dvrun_emp, 
+			v_nombre_empleado, 
+			v_nombre_usuario, 
+			v_clave_usuario
+		
+		FROM empleado NATURAL JOIN comuna
+		
+		WHERE numrun_emp = v_run;
 
-INSERT INTO usuario_clave 
-VALUES (v_mes_anno, v_numrun_emp, v_dvrun_emp, v_nombre_empleado, v_nombre_usuario, v_clave_usuario);
-COMMIT;
+	INSERT INTO usuario_clave 
+		VALUES (v_mes_anno, 
+				v_numrun_emp, 
+				v_dvrun_emp, 
+				v_nombre_empleado, 
+				v_nombre_usuario, 
+				v_clave_usuario);
+	COMMIT;
+		
+END;
+
+-- CASO 3
+
+/*
+AHEW11
+ASEZ11
+BC1002
+BT1002
+VR1003
+*/
+
+SET SERVEROUTPUT ON
+
+DECLARE
+
+	v_anno_proceso          hist_arriendo_anual_camion.anno_proceso%TYPE := '&&ANNO_PROCESO';
+	v_nro_patente           hist_arriendo_anual_camion.nro_patente%TYPE;
+	v_valor_arriendo_dia    hist_arriendo_anual_camion.valor_arriendo_dia%TYPE;
+	v_valor_garantia_dia    hist_arriendo_anual_camion.valor_garactia_dia%TYPE;
+	v_total_veces_arrendado hist_arriendo_anual_camion.total_veces_arrendado%TYPE;
+
+BEGIN
+
+    SELECT nro_patente, 
+		valor_arriendo_dia,
+		valor_garantia_dia,
+		count(nro_patente)
+            
+        INTO v_nro_patente,
+			v_valor_arriendo_dia,
+			v_valor_garantia_dia,
+			v_total_veces_arrendado
+                
+        FROM camion NATURAL JOIN arriendo_camion
+        
+        WHERE nro_patente = '&PATENTE' 
+        AND EXTRACT(YEAR FROM fecha_ini_arriendo) = &&ANNO_PROCESO-1
+        
+        GROUP BY nro_patente, 
+                valor_arriendo_dia, 
+                valor_garantia_dia; 
+    
+    INSERT INTO hist_arriendo_anual_camion
+        VALUES (v_anno_proceso,
+                v_nro_patente, 
+                v_valor_arriendo_dia, 
+                v_valor_garantia_dia, 
+                v_total_veces_arrendado);
+        
+    IF v_total_veces_arrendado < 5 THEN
+        v_valor_arriendo_dia := v_valor_arriendo_dia*0.775;
+        v_valor_garantia_dia := v_valor_garantia_dia*0.775;
+    END IF;
+    
+    UPDATE camion
+        SET valor_arriendo_dia = v_valor_arriendo_dia,
+            valor_garantia_dia = v_valor_garantia_dia
+        WHERE nro_patente = v_nro_patente;
+                
+    COMMIT;            
+
+END;
+
+UNDEFINE ANNO_PROCESO
+UNDEFINE PATENTE
+
+-- CASO 4
+
+/*
+Pruebas:
+AA1001
+AHEW11
+ASEZ11
+BT1002
+VR1003
+*/
+
+SET SERVEROUTPUT ON
+
+DECLARE
+
+    v_patente               VARCHAR(6) := '&PATENTE';
+    v_multa_por_dia         NUMBER(5) := &MULTA_POR_DIA;
+    
+    v_anno_mes_proceso      multa_arriendo.anno_mes_proceso%TYPE;
+    v_nro_patente           multa_arriendo.nro_patente%TYPE;
+    v_fecha_ini_arriendo    multa_arriendo.fecha_ini_arriendo%TYPE;
+    v_dias_solicitado       multa_arriendo.dias_solicitado%TYPE;
+    v_fecha_devolucion      multa_arriendo.fecha_devolucion%TYPE;
+    v_dias_atraso           multa_arriendo.dias_atraso%TYPE;
+    v_valor_multa           multa_arriendo.valor_multa%TYPE;
+
+BEGIN
+
+    SELECT TO_CHAR(SYSDATE, 'YYYYMM'),
+		nro_patente,
+		fecha_ini_arriendo,
+		dias_solicitados,
+		fecha_devolucion
+        
+        INTO v_anno_mes_proceso,
+            v_nro_patente,
+            v_fecha_ini_arriendo,
+            v_dias_solicitado,
+            v_fecha_devolucion
+        
+        FROM arriendo_camion
+        
+        WHERE nro_patente = v_patente
+        AND TO_CHAR(fecha_devolucion, 'MMYYYY') = TO_CHAR(ADD_MONTHS(SYSDATE, -1), 'MMYYYY')
+        AND fecha_ini_arriendo + dias_solicitados < fecha_devolucion;  
+        
+    v_dias_atraso := v_fecha_devolucion - (v_fecha_ini_arriendo + v_dias_solicitado);
+    v_valor_multa := v_dias_atraso * v_multa_por_dia;
+    
+    INSERT INTO multa_arriendo
+        VALUES (v_anno_mes_proceso,
+                v_nro_patente,
+                v_fecha_ini_arriendo,
+                v_dias_solicitado,
+                v_fecha_devolucion,
+                v_dias_atraso,
+                v_valor_multa);
+                
+    COMMIT;            
     
 END;
