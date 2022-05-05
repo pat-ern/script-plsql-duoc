c043c82dd613543c27a66217895eccef8cf16e97
pat-ern <pat.villarroel@duocuc.cl>
2022-05-04 23:41:39 -0400

    Update actividad_2_2.sql

--- actividad_2_2.sql
+++ actividad_2_2.sql
@@ -84,4 +84,117 @@ END;
 
 -- CASO 2
 
+DROP TABLE MEDICO_SERVICIO_COMUNIDAD CASCADE CONSTRAINTS;
+
+CREATE TABLE MEDICO_SERVICIO_COMUNIDAD
+(id_med_scomun NUMBER(2) GENERATED ALWAYS AS IDENTITY MINVALUE 1 
+MAXVALUE 9999999999999999999999999999
+INCREMENT BY 1 START WITH 1
+CONSTRAINT PK_MED_SERV_COMUNIDAD PRIMARY KEY,
+ unidad VARCHAR2(50) NOT NULL,
+ run_medico VARCHAR2(15) NOT NULL,
+ nombre_medico VARCHAR2(50) NOT NULL,
+ correo_institucional VARCHAR2(25) NOT NULL,
+ total_aten_medicas NUMBER(2) NOT NULL,
+ destinacion VARCHAR2(50) NOT NULL);
+
+DECLARE
+
+    CURSOR c_medicos IS
+    SELECT 0 id_med_scomun,
+        u.nombre unidad,
+        TO_CHAR(m.med_run,'09G999G999')||'-'||m.dv_run run_medico, 
+        pnombre||' '||m.snombre||' '||m.apaterno||' '||m.amaterno nombre_medico,
+        SUBSTR(u.nombre, 0, 2)||SUBSTR(m.apaterno, -3, 2)||'@medicocktk.cl' correo_institucional,
+        COUNT(ate_id) total_aten_medicas,
+        '' destinacion
+    FROM medico m
+    JOIN unidad u ON u.uni_id = m.uni_id
+    LEFT JOIN atencion a ON m.med_run = a.med_run
+    AND EXTRACT(YEAR FROM a.fecha_atencion) = EXTRACT(YEAR FROM SYSDATE)-1
+    GROUP BY u.nombre,
+        TO_CHAR(m.med_run,'09G999G999')||'-'||m.dv_run, 
+        m.pnombre||' '||m.snombre||' '||m.apaterno||' '||m.amaterno,
+        SUBSTR(u.nombre, 0, 2)||SUBSTR(m.apaterno, -3, 2)||'@medicocktk.cl',
+        m.apaterno, ''
+    ORDER BY u.nombre,
+        m.apaterno;
+        
+    v_max_atenciones NUMBER(2) := -1;
+    
+    r_atenciones medico_servicio_comunidad%ROWTYPE;
+    
+    TYPE vt_tipo_destinaciones IS VARRAY(3) OF
+        VARCHAR(50);
+        
+    v_destinaciones vt_tipo_destinaciones := vt_tipo_destinaciones(
+        'Servicio de Atención Primaria de Urgencia (SAPU)',
+        'Hospitales del área de la Salud Pública',
+        'Centros de Salud Familiar (CESFAM)');
+
+BEGIN
+    
+    FOR reg_aten IN c_medicos LOOP
+        IF reg_aten.total_aten_medicas > v_max_atenciones THEN
+            v_max_atenciones := reg_aten.total_aten_medicas;
+        END IF;
+    END LOOP;
+
+    OPEN c_medicos;
+    
+    LOOP
+    
+        FETCH c_medicos INTO r_atenciones;
+        
+        EXIT WHEN c_medicos%NOTFOUND;
+        
+        IF r_atenciones.unidad = 'ATENCIÓN AMBULATORIA' OR r_atenciones.unidad = 'ATENCIÓN ADULTO' THEN
+            r_atenciones.destinacion := v_destinaciones(1);
+        ELSIF r_atenciones.unidad = 'ATENCIÓN URGENCIA' THEN
+            IF r_atenciones.total_aten_medicas BETWEEN 0 AND 3 THEN
+                r_atenciones.destinacion := v_destinaciones(1);
+            ELSIF r_atenciones.total_aten_medicas > 3 THEN
+                r_atenciones.destinacion := v_destinaciones(2);
+            END IF;
+        ELSIF r_atenciones.unidad = 'CARDIOLOGÍA' OR r_atenciones.unidad = 'ONCOLÓGICA' THEN
+            r_atenciones.destinacion := v_destinaciones(2);
+        ELSIF r_atenciones.unidad = 'CIRUGÍA' OR r_atenciones.unidad = 'CIRUGÍA PLÁSTICA' THEN
+            IF r_atenciones.total_aten_medicas BETWEEN 0 AND 3 THEN
+                r_atenciones.destinacion := v_destinaciones(1);
+            ELSIF r_atenciones.total_aten_medicas > 3 THEN
+                r_atenciones.destinacion := v_destinaciones(2);
+            END IF;
+        ELSIF r_atenciones.unidad = 'PACIENTE CRÍTICO' THEN
+            r_atenciones.destinacion := v_destinaciones(2);
+        ELSIF r_atenciones.unidad = 'PSIQUIATRÍA Y SALUD MENTAL' THEN
+            r_atenciones.destinacion := v_destinaciones(3);
+        ELSIF r_atenciones.unidad = 'TRAUMATOLOGÍA ADULTO' THEN
+            IF r_atenciones.total_aten_medicas BETWEEN 0 AND 3 THEN
+                r_atenciones.destinacion := v_destinaciones(1);
+            ELSIF r_atenciones.total_aten_medicas > 3 THEN
+                r_atenciones.destinacion := v_destinaciones(2);
+            END IF;
+        END IF;
+
+        INSERT INTO medico_servicio_comunidad (
+                unidad,
+                run_medico,
+                nombre_medico,
+                correo_institucional,
+                total_aten_medicas,
+                destinacion)
+            VALUES (
+                r_atenciones.unidad,
+                r_atenciones.run_medico,
+                r_atenciones.nombre_medico,
+                r_atenciones.correo_institucional,
+                r_atenciones.total_aten_medicas,
+                r_atenciones.destinacion);
+    
+    END LOOP;
+    
+    CLOSE c_medicos;
+
+END;
+
 -- CASO 3
