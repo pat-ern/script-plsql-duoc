f4b0bf5e142e40f08bc212c1ac6e2cb97930de48
Patricio Villarroel <pat.villarroel@duocuc.cl>
2022-06-20 11:21:38 -0400

    totalizadores 3.2.2 y nuevo plan

--- EA3/act_3_1_2/main.sql
+++ EA3/act_3_1_2/main.sql
@@ -12,35 +12,35 @@ DECLARE
         ORDER BY id_edif, nro_depto;
 
     v_fondo_reserva gasto_comun.fondo_reserva_gc%TYPE;
-    
+    v_prorrateo NUMBER(6);
+    v_dias_atraso NUMBER(4);
+    v_multa_atraso gasto_comun.multa_gc%TYPE;
     v_valor_servicio gasto_comun.servicio_gc%TYPE;
-    
     v_tot_gasto_comun gasto_comun.monto_total_gc%TYPE;
-
+    
 BEGIN
 
     FOR i IN c_gasto_comun(:b_annomes) LOOP
     
         v_fondo_reserva := ROUND(fn_prorrateo_depto(i.anno_mes_pcgc, i.id_edif, i.nro_depto) * 5/100);
+        v_valor_servicio := i.lavanderia_gc + i.evento_gc;
+        v_prorrateo := fn_prorrateo_depto(i.anno_mes_pcgc, i.id_edif, i.nro_depto);
+        v_dias_atraso := fn_dias_atraso(i.anno_mes_pcgc, i.id_edif, i.nro_depto);
+        v_multa_atraso := fn_multa_atraso(i.anno_mes_pcgc, i.id_edif, i.nro_depto, v_dias_atraso);
         
-        SELECT  lavanderia_gc + evento_gc
-            INTO v_valor_servicio
-            FROM gasto_comun
-            WHERE anno_mes_pcgc = i.anno_mes_pcgc
-            AND id_edif = i.id_edif
-            AND nro_depto = i.nro_depto;
+        v_tot_gasto_comun := v_prorrateo + v_fondo_reserva + i.agua_individual_gc + 
+            i.combustible_individual_gc + v_valor_servicio + i.monto_atrasado_gc +
+            v_multa_atraso;
             
-        -- Totalizar gasto comun
-       
         DBMS_OUTPUT.PUT_LINE('-------------------------------------------------');
         DBMS_OUTPUT.PUT_LINE('Periodo '||i.anno_mes_pcgc||' Id Edificio '||i.id_edif||' Nro Depto '||i.nro_depto); -- DEPTO
-        DBMS_OUTPUT.PUT_LINE('Prorrateo Depto '||fn_prorrateo_depto(i.anno_mes_pcgc, i.id_edif, i.nro_depto)||' Fondo Reserva '||v_fondo_reserva);
-        DBMS_OUTPUT.PUT_LINE('Dias atraso '||fn_dias_atraso(i.anno_mes_pcgc, i.id_edif, i.nro_depto));
-        DBMS_OUTPUT.PUT_LINE('Multa atraso '||fn_multa_atraso(i.anno_mes_pcgc, i.id_edif, i.nro_depto, fn_dias_atraso(i.anno_mes_pcgc, i.id_edif, i.nro_depto)));
+        DBMS_OUTPUT.PUT_LINE('-------------------------------------------------');
+        DBMS_OUTPUT.PUT_LINE('Prorrateo Depto '||v_prorrateo);
+        DBMS_OUTPUT.PUT_LINE('Fondo Reserva '||v_fondo_reserva);
+        DBMS_OUTPUT.PUT_LINE('Multa atraso '||v_multa_atraso);
         DBMS_OUTPUT.PUT_LINE('Valor Servicio '||v_valor_servicio);
-        
-        
-        
+        DBMS_OUTPUT.PUT_LINE('Total Gasto Comun '||v_tot_gasto_comun);
+
     END LOOP;
 
 END;
--- avance.md
+++ avance.md
@@ -1,22 +1,24 @@
 # Avance
 
-## Procedimientos almacenados
+## Procedimientos
 [x] 3.1.1
 [ ] 3.1.2	
 
-## Funciones almacenadas
+## Funciones 
 [ ] 3.2.1	
-[ ] 3.2.2   15-06
 
-## Packages
-[ ] 3.3.1	17-06
-[ ] 3.3.2	19-06
+## 20-06 
+[ ] 3.2.2   Funciones 
+[ ] 3.3.1	Packages
+
+## 21-06
+[ ] 3.4.1	Triggers
+[ ] 3.3.2	Packages
 
 ## Triggers
-[ ] 3.4.1	21-06
-[ ] 3.4.2	23-06
-[ ] 3.4.3	25-06
+[ ] 3.4.2	22-06
+[ ] 3.4.3	24-06
 
 ## SQL Dinamico
-[ ] 3.5.1	27-06
-[ ] 3.5.2	29-06
+[ ] 3.5.1	25-06
+[ ] 3.5.2	27-06
